cb72d66298f583f1693f37dcf3cf155af26f6f02
voozaatracking <imvest-managment@outlook.de>
2026-02-12 16:04:00 +0100

    Add compiler and env configurations to nextConfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: false,
+  },
+  env: {
+    CHARSET: 'utf-8',
+  },
 }
 
 module.exports = nextConfig
